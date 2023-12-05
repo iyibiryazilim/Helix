36c7c283edc65fd1501d2d8779b67fecc7a02cfc
Umut Sönmez <00umutsonmez00@gmail.com>
2023-12-05 12:30:47 +0300

    workstation datastore added

---
+++ src/Core/DataAccess/Helix.Tiger.DataAccess/DataStores/WorkstationDataStore.cs
@@ -0,0 +1,29 @@
+﻿using Helix.Models;
+using Helix.Tiger.DataAccess.DataStores.Base;
+using Helix.Tiger.DataAccess.Services;
+using Microsoft.Extensions.Configuration;
+using Shared.Entity.Models;
+
+namespace Helix.Tiger.DataAccess.DataStores;
+
+public class WorkstationDataStore : BaseDataStore, IWorkstationService
+{
+	public WorkstationDataStore(IConfiguration configuration) : base(configuration)
+	{
+	}
+
+	public Task<DataResult<BankAccount>> GetWorkstationByCode(string code)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<DataResult<Workstation>> GetWorkstationById(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<DataResult<IEnumerable<Workstation>>> GetWorkstations()
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ src/Core/DataAccess/Helix.Tiger.DataAccess/Services/IWorkstationService.cs
@@ -0,0 +1,11 @@
+﻿using Helix.Models;
+using Shared.Entity.Models;
+
+namespace Helix.Tiger.DataAccess.Services;
+
+public interface IWorkstationService
+{
+	public Task<DataResult<IEnumerable<Workstation>>> GetWorkstations();
+	public Task<DataResult<Workstation>> GetWorkstationById(int id);
+	public Task<DataResult<BankAccount>> GetWorkstationByCode(string code);
+}
