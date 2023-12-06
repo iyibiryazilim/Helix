9e2f2f2dee5377ce215e0094d5b394387e0e2018
Canberk Bekiroğlu <boom.canberk@gmail.com>
2023-12-06 10:31:56 +0300

    product v2

---
+++ src/Core/DataAccess/Helix.Tiger.DataAccess/Services/IFixedAssetProductService.cs
@@ -0,0 +1,12 @@
+﻿using Helix.SharedEntity.BaseModels;
+using Helix.SharedEntity.Models;
+
+namespace Helix.Tiger.DataAccess.Services
+{
+	public interface IFixedAssetProductService
+	{
+		public Task<DataResult<IEnumerable<FixedAssetProduct>>> GetProductList();
+		public Task<DataResult<FixedAssetProduct>> GetProductById(int id);
+		public Task<DataResult<FixedAssetProduct>> GetProductByCode(string code);
+	}
+}
---
+++ src/Core/DataAccess/Helix.Tiger.DataAccess/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Helix.SharedEntity.BaseModels;
+
+namespace Helix.Tiger.DataAccess.Services
+{
+	public interface IProductService
+	{
+		public Task<DataResult<IEnumerable<Product>>> GetProductList();
+		public Task<DataResult<Product>> GetProductById(int id);
+		public Task<DataResult<Product>> GetProductByCode(string code);
+	}
+}
