78892969468989831fbec0a98e50161457cd35c6
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2023-12-07 17:18:56 +0300

    Tiger.DataAccess'e ProductionOrder'ın servisi ve datastore'u eklendi,. Production  mikroservisinde  Workstation'da fonksiyon adı değiştirildi

---
+++ src/Core/DataAccess/Helix.Tiger.DataAccess/Services/IProductionOrderService.cs
@@ -0,0 +1,11 @@
+﻿using Helix.SharedEntity.BaseModels;
+using Helix.SharedEntity.Models;
+
+namespace Helix.Tiger.DataAccess.Services;
+
+public interface IProductionOrderService
+{
+	public Task<DataResult<IEnumerable<ProductionOrder>>> GetProductionOrderList();
+	public Task<DataResult<ProductionOrder>> GetProductionOrderById(int id);
+	public Task<DataResult<ProductionOrder>> GetProductionOrderByCode(string code);
+}
