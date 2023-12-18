99858d5e0e34f1db3a34a97ace754c5576189c1b
Umut Sönmez <00umutsonmez00@gmail.com>
2023-12-18 09:44:52 +0300

    httpClientService

---
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Helpers/HttpClientHelper/IHttpClientService.cs
@@ -0,0 +1,8 @@
+﻿namespace Helix.UI.Mobile.Helpers.HttpClientHelper;
+
+public interface IHttpClientService
+{
+	HttpClient GetOrCreateHttpClient();
+	string Token { get; set; }
+	string BaseUri { get; set; }
+}
