1b1f27c1bb4438100a859fa09bf42ebda022e629
Umut Sönmez <00umutsonmez00@gmail.com>
2023-12-18 08:46:42 +0300

    MauiProgram.cs düzenlendi

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/MauiProgram.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/MauiProgram.cs
@@ -11,6 +11,18 @@ namespace Helix.UI.Mobile
             builder
                 .UseMauiApp<App>()
                 .UseMauiCommunityToolkit()
+				.ReturnRegisterServices()
+				.ReturnRegisterViewModels()
+				.ReturnRegisterViews()
+				.SalesRegisterServices()
+				.SalesRegisterViewModels()
+				.SalesRegisterViews()
+				.PurchaseRegisterViews()
+				.PurchaseRegisterViewModels()
+				.PurchaseRegisterServices()
+				.ProductRegisterServices()
+				.ProductRegisterViewModels()
+				.ProductRegisterViews()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -18,10 +30,71 @@ namespace Helix.UI.Mobile
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
         }
-    }
+
+		#region SalesModule
+		public static MauiAppBuilder SalesRegisterViews(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		public static MauiAppBuilder SalesRegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		public static MauiAppBuilder SalesRegisterServices(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		#endregion
+
+		#region PurchaseModule
+		public static MauiAppBuilder PurchaseRegisterViews(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		public static MauiAppBuilder PurchaseRegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		public static MauiAppBuilder PurchaseRegisterServices(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		#endregion
+
+		#region ProductModule
+		public static MauiAppBuilder ProductRegisterViews(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		public static MauiAppBuilder ProductRegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		public static MauiAppBuilder ProductRegisterServices(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		#endregion
+
+		#region ReturnModule
+		public static MauiAppBuilder ReturnRegisterViews(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		public static MauiAppBuilder ReturnRegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		public static MauiAppBuilder ReturnRegisterServices(this MauiAppBuilder mauiAppBuilder)
+		{
+			return mauiAppBuilder;
+		}
+		#endregion
+
+	}
 }
