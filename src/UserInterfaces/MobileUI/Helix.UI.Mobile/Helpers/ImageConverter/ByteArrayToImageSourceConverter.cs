b5403423238618abe270226d395ed0eacccd424a
Canberk Bekiroğlu <boom.canberk@gmail.com>
2023-12-20 14:34:50 +0300

    Current Content view

---
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Helpers/ImageConverter/ByteArrayToImageSourceConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Helix.UI.Mobile.Helpers.ImageConverter
+{
+	public class ByteArrayToImageSourceConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			byte[] bytes;
+
+			if (value != null)
+			{
+				bytes = System.Convert.FromBase64String(value.ToString());
+				ImageSource imageSource = ImageSource.FromStream(() => new MemoryStream(bytes));
+				return imageSource;
+			}
+			else
+			{
+				string resourceName = $"{FileSystem.Current.AppDataDirectory}/Resources/Images/notfoundimage.png";
+				ImageSource imageSource = ImageSource.FromFile(resourceName);
+				return imageSource;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
