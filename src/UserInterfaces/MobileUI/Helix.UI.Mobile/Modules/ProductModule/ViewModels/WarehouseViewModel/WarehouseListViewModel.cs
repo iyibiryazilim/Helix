b6a6c282c54dd90a2362c6c1123de1fd024e649d
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2023-12-21 12:38:39 +0300

    Warehouse

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/WarehouseViewModel/WarehouseListViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/WarehouseViewModel/WarehouseListViewModel.cs
@@ -1,85 +1,146 @@
-﻿using Helix.UI.Mobile.Helpers.HttpClientHelper;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Helix.UI.Mobile.Helpers.HttpClientHelper;
 using Helix.UI.Mobile.Modules.ProductModule.Models;
 using Helix.UI.Mobile.Modules.ProductModule.Services;
-using Helix.UI.Mobile.Modules.SalesModule.Services;
 using Helix.UI.Mobile.MVVMHelper;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 
-namespace Helix.UI.Mobile.Modules.ProductModule.ViewModels.WarehouseViewModel
+namespace Helix.UI.Mobile.Modules.ProductModule.ViewModels.WarehouseViewModel;
+public partial class WarehouseListViewModel : BaseViewModel
 {
-    public partial class WarehouseListViewModel : BaseViewModel
-    {
-        IHttpClientService _httpClientService;
-        private readonly IWarehouseService _warehouseService;
-        public ObservableCollection<Warehouse> Items { get; } = new();
-        public Command GetWarehousesCommand { get; }
+	IHttpClientService _httpClientService;
+	private readonly IWarehouseService _warehouseService;
 
-        public WarehouseListViewModel(IHttpClientService httpClientService, IWarehouseService warehouseService)
-        {
-            _httpClientService = httpClientService;
-            _warehouseService = warehouseService;
-            GetWarehousesCommand= new Command(async () => await LoadData());
-        }
-        async Task LoadData()
-        {
-            if (IsBusy)
-                return;
-            try
-            {
-                await Task.Delay(500);
-                await MainThread.InvokeOnMainThreadAsync(GetWarehousesAsync);
+	[ObservableProperty]
+	string searchText = string.Empty;
+	[ObservableProperty]
+	int currentPage = 20;
+	[ObservableProperty]
+	int pageSize = 0;
 
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-                await Shell.Current.DisplayAlert("Warehouse Error: ", $"{ex.Message}", "Tamam");
-            }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
+	public ObservableCollection<Warehouse> Items { get; } = new();
+	public Command GetWarehousesCommand { get; }
+	public Command<string> PerformSearchCommand { get; }
+	
+	public WarehouseListViewModel(IHttpClientService httpClientService, IWarehouseService warehouseService)
+	{
+		Title = "Ambarlar";
+		_httpClientService = httpClientService;
+		_warehouseService = warehouseService;
+		GetWarehousesCommand = new Command(async () => await LoadData());
+		PerformSearchCommand = new Command<string>(async (searchText) => await PerformSearchAsync(searchText));
+		
+	}
+	async Task LoadData()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			await Task.Delay(500);
+			await MainThread.InvokeOnMainThreadAsync(GetWarehousesAsync);
 
-        async Task GetWarehousesAsync()
-        {
-            if(IsBusy) return;
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Warehouse Error: ", $"{ex.Message}", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
 
-            try
-            {
-                IsBusy = true;
-                IsRefreshing = true;
-                var httpClient=_httpClientService.GetOrCreateHttpClient();
-                var result = await _warehouseService.GetObjects(httpClient);
-                if (Items.Any())
-                {
-                    Items.Clear();
-                    foreach (Warehouse item in result.Data)
-                    {
-                        Items.Add(item);
-                    }
-                }
-            }
+	async Task GetWarehousesAsync()
+	{
+		if (IsBusy) return;
 
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-                await Shell.Current.DisplayAlert("Warehouse Error:", $"{ex.Message}", "Tamam");
-            }
-            finally
-            {
-                IsBusy = false;
-                IsRefreshing = false;
-            }
+		try
+		{
+			IsBusy = true;
+			IsRefreshing = true;
+			var httpClient = _httpClientService.GetOrCreateHttpClient();
+			var result = await _warehouseService.GetObjects(httpClient);
+			if (result.Data.Any())
+			{
+				Items.Clear();
+				foreach (Warehouse item in result.Data)
+				{
+					Items.Add(item);
+				}
+			}
+		}
 
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Warehouse Error:", $"{ex.Message}", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
 
+	async Task PerformSearchAsync(string text)
+	{
+		if (IsBusy) return;
 
+		try
+		{
+			IsBusy = true;
 
-        }
-       
+			if(!string.IsNullOrEmpty(text))
+			{
+				if(text.Length >= 3)
+				{
+					SearchText = text;
+					await ReloadAsync();
+				}
+			}
+		} 
+		catch(Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Search Error:", $"{ex.Message}", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
 
+	async Task ReloadAsync()
+	{
+		try
+		{
+			IsBusy = true;
+			IsRefreshing = true;
 
+			var httpClient = _httpClientService.GetOrCreateHttpClient();
+			var result = await _warehouseService.GetObjects(httpClient);
 
-    }
+			if(result.Data.Any())
+			{
+				Items.Clear();
+
+				foreach(var item in result.Data)
+				{
+					await Task.Delay(100);
+					Items.Add(item);
+				}
+			}
+		} catch(Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Reload Error:", $"{ex.Message}", "Tamam");
+		} finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
 }
