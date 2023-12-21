e5262b641b85f61eac27bd5ee8a6f2817a2655c7
Canberk Bekiroğlu <boom.canberk@gmail.com>
2023-12-21 12:13:10 +0300

    Current Content View finished functionality

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViews/CurrentListView.xaml.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViews/CurrentListView.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Mvvm.Input;
 using Helix.UI.Mobile.Modules.BaseModule.Models;
 using System.Collections.ObjectModel;
 
@@ -5,14 +6,70 @@ namespace Helix.UI.Mobile.Modules.BaseModule.SharedViews;
 
 public partial class CurrentListView : ContentView
 {
+	//List
 	public static readonly BindableProperty CurrentListProperty = BindableProperty.Create(nameof(CurrentList), typeof(ObservableCollection<Current>), typeof(CurrentListView), null);
+
+
+	//Commands
+	public static readonly BindableProperty SearchCommandProperty = BindableProperty.Create(nameof(SearchCommand), typeof(Command), typeof(CurrentListView), null);
+	public static readonly BindableProperty LoadMoreCommandProperty = BindableProperty.Create(nameof(LoadMoreCommand), typeof(AsyncRelayCommand), typeof(CurrentListView), null);
+	public static readonly BindableProperty SortCommandProperty = BindableProperty.Create(nameof(SortCommand), typeof(AsyncRelayCommand), typeof(CurrentListView), null);
+	public static readonly BindableProperty ReloadCommandProperty = BindableProperty.Create(nameof(ReloadCommand), typeof(AsyncRelayCommand), typeof(CurrentListView), null);
+
+	//Properties
+	public static readonly BindableProperty IsRefreshingProperty = BindableProperty.Create(nameof(IsRefreshing), typeof(bool), typeof(CurrentListView), false);
+	public static readonly BindableProperty IsBusyProperty = BindableProperty.Create(nameof(IsBusy), typeof(bool), typeof(CurrentListView), false);
+
+
+	//List
 	public ObservableCollection<Current> CurrentList
 	{
 		get => GetValue(CurrentListProperty) as ObservableCollection<Current>;
 		set => SetValue(CurrentListProperty, value);
 	}
+	//Commands
+	public Command SearchCommand
+	{
+		get => GetValue(SearchCommandProperty) as Command;
+		set => SetValue(SearchCommandProperty, value);
+	}
+	public AsyncRelayCommand LoadMoreCommand
+	{
+
+		get => GetValue(LoadMoreCommandProperty) as AsyncRelayCommand;
+		set => SetValue(LoadMoreCommandProperty, value);
+
+	}
+	public AsyncRelayCommand SortCommand
+	{
+
+		get => GetValue(SortCommandProperty) as AsyncRelayCommand;
+		set => SetValue(SortCommandProperty, value);
+
+	}
+	public AsyncRelayCommand ReloadCommand
+	{
+
+		get => GetValue(ReloadCommandProperty) as AsyncRelayCommand;
+		set => SetValue(ReloadCommandProperty, value);
+
+	}
+
+	//Properties
+	public bool IsRefreshing
+	{
+		get => (bool)GetValue(IsRefreshingProperty);
+		set => SetValue(IsRefreshingProperty, value);
+	}
+	public bool IsBusy
+	{
+		get => (bool)GetValue(IsBusyProperty);
+		set => SetValue(IsBusyProperty, value);
+	}
 	public CurrentListView()
 	{
 		InitializeComponent();
 	}
+
+
 }
--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/SalesModule/ViewModels/CustomerViewModels/CustomerListViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/SalesModule/ViewModels/CustomerViewModels/CustomerListViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Helix.UI.Mobile.Helpers.HttpClientHelper;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Helix.UI.Mobile.Helpers.HttpClientHelper;
 using Helix.UI.Mobile.Modules.BaseModule.Models;
 using Helix.UI.Mobile.Modules.SalesModule.DataStores;
 using Helix.UI.Mobile.Modules.SalesModule.Models;
@@ -13,13 +15,29 @@ namespace Helix.UI.Mobile.Modules.SalesModule.ViewModels.CustomerViewModels
 	{
 		IHttpClientService _httpClientService;
 		private readonly ICustomerService _customerService;
+		//Lists
 		public ObservableCollection<Current> Items { get; } = new();
+
+		//Commands
 		public Command GetCustomersCommand { get; }
+		public Command SearchCommand { get; }
+
+		//Properties
+		[ObservableProperty]
+		string searchText = string.Empty;
+		[ObservableProperty]
+		CustomerOrderBy orderBy = CustomerOrderBy.nameasc;
+		[ObservableProperty]
+		int currentPage = 0;
+		[ObservableProperty]
+		int pageSize = 20;
 		public CustomerListViewModel(IHttpClientService httpClientService, ICustomerService customerService)
 		{
 			_httpClientService = httpClientService;
 			_customerService = customerService;
 			GetCustomersCommand = new Command(async () => await LoadData());
+			SearchCommand = new Command<string>(async (searchText) => await PerformSearchAsync(searchText));
+
 		}
 
 		async Task LoadData()
@@ -29,7 +47,8 @@ namespace Helix.UI.Mobile.Modules.SalesModule.ViewModels.CustomerViewModels
 			try
 			{
 				await Task.Delay(500);
-				await MainThread.InvokeOnMainThreadAsync(GetCustomersAsync);
+				//await MainThread.InvokeOnMainThreadAsync(ReloadAsync());
+				await ReloadAsync();
 
 			}
 			catch (Exception ex)
@@ -42,27 +61,145 @@ namespace Helix.UI.Mobile.Modules.SalesModule.ViewModels.CustomerViewModels
 				IsBusy = false;
 			}
 		}
-		async Task GetCustomersAsync()
+
+		public async Task PerformSearchAsync(string text)
 		{
 			if (IsBusy)
 				return;
 			try
 			{
 				IsBusy = true;
+				if (!string.IsNullOrEmpty(text))
+				{
+					if (text.Length >= 3)
+					{
+						SearchText = text;
+						await ReloadAsync();
+					}
+				}
+				else
+				{
+					SearchText = string.Empty;
+					await ReloadAsync();
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+
+		[RelayCommand]
+		async Task LoadMoreAsync()
+		{
+			if (IsBusy)
+				return;
+			try
+			{
+				IsBusy = true;
+				var httpClient = _httpClientService.GetOrCreateHttpClient();
+
+				CurrentPage++;
+				var result = await _customerService.GetObjects(httpClient, SearchText, OrderBy, CurrentPage, PageSize);
+				if (result.Data.Any())
+				{
+					foreach (Customer item in result.Data)
+					{
+						await Task.Delay(50);
+						Items.Add(item);
+					}
+				}
+				else
+				{
+					CurrentPage--;
+				}
+
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+				await Shell.Current.DisplayAlert("Customer Error: ", $"{ex.Message}", "Tamam");
+
+			}
+			finally
+			{
+				IsBusy = false;
+				IsRefreshing = false;
+			}
+		}
+
+		[RelayCommand]
+		async Task ReloadAsync()
+		{
+			try
+			{
+				IsBusy = true;
 				IsRefreshing = true;
 				var httpClient = _httpClientService.GetOrCreateHttpClient();
 
-				var result = await _customerService.GetObjects(httpClient,"",CustomerOrderBy.nameasc,0,20);
-
+				CurrentPage = 0;
+				var result = await _customerService.GetObjects(httpClient, SearchText, OrderBy, CurrentPage, PageSize);
 				if (result.Data.Any())
 				{
 					Items.Clear();
 					foreach (Customer item in result.Data)
 					{
+						await Task.Delay(100);
 						Items.Add(item);
 					}
 				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+				await Shell.Current.DisplayAlert("Customer Error: ", $"{ex.Message}", "Tamam");
+			}
+			finally
+			{
+				IsBusy = false;
+				IsRefreshing = false;
+			}
+		}
+		[RelayCommand]
+		async Task SortAsync()
+		{
+			if (IsBusy) return;
+			try
+			{
+				string response = await Shell.Current.DisplayActionSheet("Sırala", "Vazgeç", null, "Kod A-Z", "Kod Z-A", "Ad A-Z", "Ad Z-A");
+				if (!string.IsNullOrEmpty(response))
+				{
+					CurrentPage = 0;
+					await Task.Delay(100);
+					switch (response)
+					{
+						case "Kod A-Z":
+							OrderBy = CustomerOrderBy.codeasc;
+							await ReloadAsync();
+							break;
+						case "Kod Z-A":
+							OrderBy = CustomerOrderBy.codedesc;
+							await ReloadAsync();
+							break;
+						case "Ad A-Z":
+							OrderBy = CustomerOrderBy.nameasc;
+							await ReloadAsync();
+							break;
+						case "Ad Z-A":
+							OrderBy = CustomerOrderBy.namedesc;
+							await ReloadAsync();
+							break;
+						default:
+							await Shell.Current.DisplayAlert("Customer Error: ", "Yanlış Girdi", "Tamam");
+							await ReloadAsync();
+							break;
 
+					}
+				}
 			}
 			catch (Exception ex)
 			{
