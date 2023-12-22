585845697b6200e5592c611e701b71696e89a1ad
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2023-12-22 16:09:30 +0300

    hızlı işlemler bottom sheet

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/ProductViewModel/ProductDetailViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/ProductViewModel/ProductDetailViewModel.cs
@@ -1,17 +1,49 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Helix.UI.Mobile.Modules.ProductModule.Models;
+using Helix.UI.Mobile.Modules.ProductModule.ViewModels.ProductViewModel.BottomSheetViewModels;
+using Helix.UI.Mobile.Modules.ProductModule.Views.ProductViews.BottomSheetViews;
 using Helix.UI.Mobile.MVVMHelper;
+using System.Diagnostics;
 
 namespace Helix.UI.Mobile.Modules.ProductModule.ViewModels.ProductViewModel;
 
 [QueryProperty(name: nameof(Product), queryId: nameof(Product))]
 public partial class ProductDetailViewModel : BaseViewModel
 {
+	IServiceProvider _serviceProvider;
+
 	[ObservableProperty]
 	Product product;
 
-	public ProductDetailViewModel()
+	public ProductDetailViewModel(IServiceProvider serviceProvider)
 	{
 		Title = "Ürün Detayı";
+		_serviceProvider = serviceProvider;
+	}
+
+	[RelayCommand]
+	async Task OpenFastOperationBottomSheetAsync()
+	{
+		if(IsBusy)
+			return;
+		try
+		{
+			IsBusy = true;
+
+			ProductFastOperationBottomSheetViewModel viewModel = _serviceProvider.GetService<ProductFastOperationBottomSheetViewModel>();
+
+			ProductFastOperationBottomSheetView sheet = new ProductFastOperationBottomSheetView(viewModel);
+			await sheet.ShowAsync();
+		}
+		catch(Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error :", ex.Message, "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
 	}
 }
---
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/Views/ProductViews/BottomSheetViews/ProductFastOperationBottomSheetView.xaml.cs
@@ -0,0 +1,14 @@
+using Helix.UI.Mobile.Modules.ProductModule.ViewModels.ProductViewModel.BottomSheetViewModels;
+using The49.Maui.BottomSheet;
+
+namespace Helix.UI.Mobile.Modules.ProductModule.Views.ProductViews.BottomSheetViews;
+
+public partial class ProductFastOperationBottomSheetView : BottomSheet
+{
+	ProductFastOperationBottomSheetViewModel _viewModel;
+	public ProductFastOperationBottomSheetView(ProductFastOperationBottomSheetViewModel viewModel)
+	{
+		InitializeComponent();
+		BindingContext = _viewModel = viewModel;
+	}
+}
