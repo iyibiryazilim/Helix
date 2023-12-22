7f10f0fd0cb81fb70ae6b60378250164db3fd32c
Canberk Bekiroğlu <boom.canberk@gmail.com>
2023-12-22 15:53:29 +0300

    CurrentDetail

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViews/CurrentDetailContentView.xaml.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViews/CurrentDetailContentView.xaml.cs
@@ -1,7 +1,29 @@
+using Helix.UI.Mobile.Modules.BaseModule.Models;
+
 namespace Helix.UI.Mobile.Modules.BaseModule.SharedViews;
 
 public partial class CurrentDetailContentView : ContentView
 {
+	//Properties
+	public static readonly BindableProperty IsRefreshingProperty = BindableProperty.Create(nameof(IsRefreshing), typeof(bool), typeof(CurrentDetailContentView), false);
+	public static readonly BindableProperty IsBusyProperty = BindableProperty.Create(nameof(IsBusy), typeof(bool), typeof(CurrentDetailContentView), false);
+	public static readonly BindableProperty CurrentProperty = BindableProperty.Create(nameof(Current), typeof(Current), typeof(CurrentDetailContentView), null);
+	//Properties
+	public bool IsRefreshing
+	{
+		get => (bool)GetValue(IsRefreshingProperty);
+		set => SetValue(IsRefreshingProperty, value);
+	}
+	public bool IsBusy
+	{
+		get => (bool)GetValue(IsBusyProperty);
+		set => SetValue(IsBusyProperty, value);
+	}
+	public Current Current
+	{
+		get => (Current)GetValue(CurrentProperty);
+		set => SetValue(CurrentProperty, value);
+	}
 	public CurrentDetailContentView()
 	{
 		InitializeComponent();
--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/SalesModule/ViewModels/CustomerViewModels/CustomerDetailViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/SalesModule/ViewModels/CustomerViewModels/CustomerDetailViewModel.cs
@@ -1,10 +1,31 @@
-﻿using Helix.UI.Mobile.Modules.BaseModule.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Helix.UI.Mobile.Helpers.HttpClientHelper;
+using Helix.UI.Mobile.Modules.BaseModule.Models;
+using Helix.UI.Mobile.Modules.SalesModule.Models;
 using Helix.UI.Mobile.MVVMHelper;
 
 namespace Helix.UI.Mobile.Modules.SalesModule.ViewModels.CustomerViewModels
 {
-	[QueryProperty(nameof(Current),nameof(Current))]
+	[QueryProperty(nameof(Current), nameof(Current))]
 	public partial class CustomerDetailViewModel : BaseViewModel
 	{
+		IHttpClientService _httpClientService;
+
+		[ObservableProperty]
+		Customer current;
+
+		public CustomerDetailViewModel(IHttpClientService httpClientService)
+		{
+			Title = "Müşteri Detayı";
+			_httpClientService = httpClientService;
+
+		}
+
+		[RelayCommand]
+		async Task GoToBackAsync()
+		{
+			await Shell.Current.GoToAsync("..");
+		}
 	}
 }
