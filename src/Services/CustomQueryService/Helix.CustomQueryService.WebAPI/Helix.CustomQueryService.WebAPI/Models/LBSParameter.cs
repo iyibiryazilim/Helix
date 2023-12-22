e185e60d3b1c2b108966bb9d87a83ab3254ee2df
Canberk Bekiroğlu <boom.canberk@gmail.com>
2023-12-22 16:54:48 +0300

    CustomQueryService Added

---
+++ src/Services/CustomQueryService/Helix.CustomQueryService.WebAPI/Helix.CustomQueryService.WebAPI/Models/LBSParameter.cs
@@ -0,0 +1,42 @@
+﻿namespace Helix.CustomQueryService.WebAPI.Models
+{
+	public class LBSParameter
+	{
+		#region Veri Tabanı Erişim Bilgileri
+		public string DataSource { get; set; } = string.Empty;
+		public string UserId { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+		public string InitialCatalog { get; set; } = string.Empty;
+		public bool IsMasterDb { get; set; } = default;
+		public string MasterDb { get; set; } = string.Empty;
+
+		#endregion
+
+		#region Logo Firma Bilgileri
+		/// <summary>
+		/// Logo Firma Bilgisi
+		/// </summary>
+		public int DefaultFirmNumber { get; set; } = default;
+		/// <summary>
+		/// Logo Dönem Bilgisi
+		/// </summary>
+		public int DefaultPeriodNumber { get; set; } = default;
+		#endregion
+
+		#region Logo Kullanıcı Bilgileri
+		/// <summary>
+		/// Logo Kullanıcı Adı
+		/// </summary>
+		public string LBS_UserName { get; set; } = string.Empty;
+		/// <summary>
+		/// Logo Kullanıcı Şifresi
+		/// </summary>
+		public string LBS_Password { get; set; } = string.Empty;
+		#endregion
+
+		public override string ToString()
+		{
+			return $"Data Source={this.DataSource}; User id= {this.UserId}; Password = {this.Password}; Initial Catalog = {this.InitialCatalog}";
+		}
+	}
+}
