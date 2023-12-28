0a8d8f0c7d2241a7cd07ddda71b2f4949da79102
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2023-12-28 16:19:46 +0300

    Malzeme sayfaları

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/ProductViewModel/ProductDetailOutputTransactionListViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/ProductViewModel/ProductDetailOutputTransactionListViewModel.cs
@@ -1,16 +1,235 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Helix.UI.Mobile.Helpers.HttpClientHelper;
 using Helix.UI.Mobile.Modules.ProductModule.Models;
+using Helix.UI.Mobile.Modules.ProductModule.Services;
 using Helix.UI.Mobile.MVVMHelper;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using static Helix.UI.Mobile.Modules.ProductModule.DataStores.ProductTransactionLineDataStore;
 
 namespace Helix.UI.Mobile.Modules.ProductModule.ViewModels.ProductViewModel;
 
 [QueryProperty(name: nameof(Product), queryId: nameof(Product))]
 public partial class ProductDetailOutputTransactionListViewModel : BaseViewModel
 {
+	IHttpClientService _httpClient;
+	IProductTransactionLineService _productTransactionLineService;
+
 	[ObservableProperty]
 	Product product;
-	public ProductDetailOutputTransactionListViewModel()
+	[ObservableProperty]
+	string searchText = string.Empty;
+	[ObservableProperty]
+	int currentPage = 0;
+	[ObservableProperty]
+	int pageSize = 20;
+	[ObservableProperty]
+	ProductTransactionLineOrderBy orderBy = ProductTransactionLineOrderBy.datedesc;
+
+	public ObservableCollection<ProductTransactionLine> ProductOutputTransactionListItems { get; } = new();
+	public Command GetProductOutputTransactionListItemsCommand { get; }
+	public Command SearchCommand { get; }
+	public ProductDetailOutputTransactionListViewModel(IHttpClientService httpClient, IProductTransactionLineService productTransactionLineService)
 	{
 		Title = "Çıkış Hareketleri";
+		_httpClient = httpClient;
+		_productTransactionLineService = productTransactionLineService;
+
+		GetProductOutputTransactionListItemsCommand = new Command(async () => await LoadData());
+		SearchCommand = new Command<string>(async (searchText) => await PerformSearchAsync(searchText));
+	}
+
+	public async Task LoadData()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			await Task.Delay(500);
+			await MainThread.InvokeOnMainThreadAsync(ReloadAsync);
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error :", ex.Message, "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	[RelayCommand]
+	public async Task LoadMoreAsync()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			IsBusy = true;
+			var httpClient = _httpClient.GetOrCreateHttpClient();
+
+			CurrentPage++;
+			var result = await _productTransactionLineService.GetTransactionLinesByTransactionType(httpClient, Product.Code, "2,3", SearchText, OrderBy, CurrentPage, PageSize);
+			if (result.Data.Any())
+			{
+				foreach (var item in result.Data)
+				{
+					await Task.Delay(50);
+					ProductOutputTransactionListItems.Add(item);
+				}
+			}
+			else
+			{
+				CurrentPage--;
+			}
+
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Product Error: ", $"{ex.Message}", "Tamam");
+
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
+
+	[RelayCommand]
+	public async Task ReloadAsync()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			IsBusy = true;
+			IsRefreshing = true;
+			var httpClient = _httpClient.GetOrCreateHttpClient();
+			CurrentPage = 0;
+
+			var result = await _productTransactionLineService.GetTransactionLinesByTransactionType(httpClient, Product.Code, "2,3", SearchText, OrderBy, CurrentPage, PageSize);
+			if (result.Data.Any())
+			{
+				ProductOutputTransactionListItems.Clear();
+				foreach (var item in result.Data)
+				{
+					await Task.Delay(100);
+					ProductOutputTransactionListItems.Add(item);
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error :", ex.Message, "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
+
+	[RelayCommand]
+	public async Task SortAsync()
+	{
+		if (IsBusy) return;
+		try
+		{
+			string response = await Shell.Current.DisplayActionSheet("Sırala", "Vazgeç", null, "Tarihe Göre Artan", "Tarihe Göre Azalan", "Miktara Göre Artan", "Miktara Göre Azalan");
+			if (!string.IsNullOrEmpty(response))
+			{
+				CurrentPage = 0;
+				await Task.Delay(100);
+				switch (response)
+				{
+					case "Tarihe Göre Artan":
+						OrderBy = ProductTransactionLineOrderBy.dateasc;
+						await ReloadAsync();
+						break;
+					case "Tarihe Göre Azalan":
+						OrderBy = ProductTransactionLineOrderBy.datedesc;
+						await ReloadAsync();
+						break;
+					case "Miktara Göre Artan":
+						OrderBy = ProductTransactionLineOrderBy.quantityasc;
+						await ReloadAsync();
+						break;
+					case "Miktara Göre Azalan":
+						OrderBy = ProductTransactionLineOrderBy.quantitydesc;
+						await ReloadAsync();
+						break;
+					default:
+						await ReloadAsync();
+						break;
+
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error: ", $"{ex.Message}", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
+
+	public async Task PerformSearchAsync(string text)
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+
+			if (!string.IsNullOrEmpty(text))
+			{
+				if (text.Length >= 3)
+				{
+					SearchText = text;
+					await ReloadAsync();
+				}
+			}
+			else
+			{
+				SearchText = string.Empty;
+				await ReloadAsync();
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	[RelayCommand]
+	public async Task GoToBackAsync()
+	{
+		try
+		{
+			IsBusy = true;
+
+			await Shell.Current.GoToAsync("..");
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error :", ex.Message, "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
 	}
 }
--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/ProductViewModel/ProductDetailSalesDispatchListViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/ProductViewModel/ProductDetailSalesDispatchListViewModel.cs
@@ -1,16 +1,235 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Helix.UI.Mobile.Helpers.HttpClientHelper;
 using Helix.UI.Mobile.Modules.ProductModule.Models;
+using Helix.UI.Mobile.Modules.ProductModule.Services;
 using Helix.UI.Mobile.MVVMHelper;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using static Helix.UI.Mobile.Modules.ProductModule.DataStores.ProductTransactionLineDataStore;
 
 namespace Helix.UI.Mobile.Modules.ProductModule.ViewModels.ProductViewModel;
 
 [QueryProperty(name: nameof(Product), queryId: nameof(Product))]
 public partial class ProductDetailSalesDispatchListViewModel : BaseViewModel
 {
+	IHttpClientService _httpClient;
+	IProductTransactionLineService _productTransactionLineService;
+
 	[ObservableProperty]
 	Product product;
-	public ProductDetailSalesDispatchListViewModel()
+	[ObservableProperty]
+	string searchText = string.Empty;
+	[ObservableProperty]
+	int currentPage = 0;
+	[ObservableProperty]
+	int pageSize = 20;
+	[ObservableProperty]
+	ProductTransactionLineOrderBy orderBy = ProductTransactionLineOrderBy.datedesc; 
+
+	public ObservableCollection<ProductTransactionLine> ProductTransactionSalesDispatchListItems { get; } = new();
+	public Command GetProductTransactionSalesDispatchListItemsCommand { get; }
+	public Command SearchCommand { get; }
+	public ProductDetailSalesDispatchListViewModel(IHttpClientService httpClient, IProductTransactionLineService productTransactionLineService)
 	{
 		Title = "Satış İrsaliyesi";
+		_httpClient = httpClient;
+		_productTransactionLineService = productTransactionLineService;
+
+		GetProductTransactionSalesDispatchListItemsCommand = new Command(async () => await LoadData());
+		SearchCommand = new Command<string>(async (searchText) => await PerformSearchAsync(searchText));
+	}
+
+	public async Task LoadData()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			await Task.Delay(500);
+			await MainThread.InvokeOnMainThreadAsync(ReloadAsync);
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error :", ex.Message, "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	[RelayCommand]
+	public async Task LoadMoreAsync()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			IsBusy = true;
+			var httpClient = _httpClient.GetOrCreateHttpClient();
+
+			CurrentPage++;
+			var result = await _productTransactionLineService.GetTransactionLinesByTransactionType(httpClient, Product.Code, "7,8", SearchText, OrderBy, CurrentPage, PageSize);
+			if (result.Data.Any())
+			{
+				foreach (var item in result.Data)
+				{
+					await Task.Delay(50);
+					ProductTransactionSalesDispatchListItems.Add(item);
+				}
+			}
+			else
+			{
+				CurrentPage--;
+			}
+
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Product Error: ", $"{ex.Message}", "Tamam");
+
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
+
+	[RelayCommand]
+	public async Task ReloadAsync()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			IsBusy = true;
+			IsRefreshing = true;
+			var httpClient = _httpClient.GetOrCreateHttpClient();
+			CurrentPage = 0;
+
+			var result = await _productTransactionLineService.GetTransactionLinesByTransactionType(httpClient, Product.Code, "7,8", SearchText, OrderBy, CurrentPage, PageSize);
+			if (result.Data.Any())
+			{
+				ProductTransactionSalesDispatchListItems.Clear();
+				foreach (var item in result.Data)
+				{
+					await Task.Delay(100);
+					ProductTransactionSalesDispatchListItems.Add(item);
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error :", ex.Message, "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
+
+	[RelayCommand]
+	public async Task SortAsync()
+	{
+		if (IsBusy) return;
+		try
+		{
+			string response = await Shell.Current.DisplayActionSheet("Sırala", "Vazgeç", null, "Tarihe Göre Artan", "Tarihe Göre Azalan", "Miktara Göre Artan", "Miktara Göre Azalan");
+			if (!string.IsNullOrEmpty(response))
+			{
+				CurrentPage = 0;
+				await Task.Delay(100);
+				switch (response)
+				{
+					case "Tarihe Göre Artan":
+						OrderBy = ProductTransactionLineOrderBy.dateasc;
+						await ReloadAsync();
+						break;
+					case "Tarihe Göre Azalan":
+						OrderBy = ProductTransactionLineOrderBy.datedesc;
+						await ReloadAsync();
+						break;
+					case "Miktara Göre Artan":
+						OrderBy = ProductTransactionLineOrderBy.quantityasc;
+						await ReloadAsync();
+						break;
+					case "Miktara Göre Azalan":
+						OrderBy = ProductTransactionLineOrderBy.quantitydesc;
+						await ReloadAsync();
+						break;
+					default:
+						await ReloadAsync();
+						break;
+
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error: ", $"{ex.Message}", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
+
+	public async Task PerformSearchAsync(string text)
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+
+			if (!string.IsNullOrEmpty(text))
+			{
+				if (text.Length >= 3)
+				{
+					SearchText = text;
+					await ReloadAsync();
+				}
+			}
+			else
+			{
+				SearchText = string.Empty;
+				await ReloadAsync();
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	[RelayCommand]
+	public async Task GoToBackAsync()
+	{
+		try
+		{
+			IsBusy = true;
+
+			await Shell.Current.GoToAsync("..");
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error :", ex.Message, "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
 	}
 }
