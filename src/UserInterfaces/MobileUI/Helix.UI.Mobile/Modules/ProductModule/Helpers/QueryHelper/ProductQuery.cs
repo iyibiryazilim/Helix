4d383823ea3b5d2eeadea5e5268ba16e8679627d
Umut Sönmez <00umutsonmez00@gmail.com>
2023-12-29 16:16:55 +0300

    mazleme paneli

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/Helpers/QueryHelper/ProductQuery.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/Helpers/QueryHelper/ProductQuery.cs
@@ -23,6 +23,71 @@ public class ProductQuery : IDisposable
 		return query;
 	}
 
+	public string GetTopProducts()
+	{
+		var query = @$"WITH DerivedTable AS (
+    SELECT
+        STLINE.STOCKREF AS ReferenceId,
+        ITEMS.CODE AS Code,
+        ITEMS.NAME AS Name,
+        UNITSET.CODE AS SubUnitsetCode,
+        UNITSET.LOGICALREF AS SubUnitsetReferenceId,
+        BASEUNITSET.CODE AS UnitsetCode,
+        BASEUNITSET.LOGICALREF AS UnitsetReferenceId,
+        MAX(STLINE.DATE_) AS MaxDate
+    FROM LG_{CompanyNumber}_{CompanyPeriod}_STLINE AS STLINE
+    LEFT JOIN LG_{CompanyNumber}_ITEMS AS ITEMS ON STLINE.STOCKREF = ITEMS.LOGICALREF
+    LEFT JOIN LG_{CompanyNumber}_UNITSETF AS BASEUNITSET ON ITEMS.UNITSETREF = BASEUNITSET.LOGICALREF
+    LEFT JOIN LG_{CompanyNumber}_UNITSETL AS UNITSET ON UNITSET.UNITSETREF = BASEUNITSET.LOGICALREF AND MAINUNIT = 1
+    GROUP BY STLINE.STOCKREF, ITEMS.CODE, ITEMS.NAME, BASEUNITSET.CODE, BASEUNITSET.LOGICALREF, UNITSET.CODE, UNITSET.LOGICALREF
+)
+SELECT TOP 5
+    ReferenceId,
+    Code,
+    Name,
+    SubUnitsetCode,
+    SubUnitsetReferenceId,
+    UnitsetCode,
+    UnitsetReferenceId,
+	FIRMDOC.LDATA as Image
+FROM DerivedTable
+LEFT JOIN LG_{CompanyNumber}_FIRMDOC AS FIRMDOC ON FIRMDOC.INFOREF = ReferenceId AND FIRMDOC.INFOTYP = 20
+ORDER BY MaxDate DESC";
+
+		return query;
+	}
+	public string LastTransactionList()
+	{
+		var query = @$"SELECT TOP 5
+        [ReferenceId] = STLINE.LOGICALREF,
+        [TransactionDate] = STLINE.DATE_,
+        [TransactionTime] = dbo.LG_INTTOTIME(STFICHE.FTIME),
+        [BaseTransactionCode] = STFICHE.FICHENO,
+        [TransactionType] = STLINE.TRCODE,
+        [ProductReferenceId] = STLINE.STOCKREF,
+        [ProductCode] = ITEMS.CODE,
+        [ProductName] = ITEMS.NAME,
+        [SubUnitset] = UNITSET.CODE,
+        [SubUnitsetReferenceId] = UNITSET.LOGICALREF,
+        [UnitsetCode] = BASEUNITSET.CODE,
+        [UnitsetReferenceId] = BASEUNITSET.LOGICALREF,
+        [Quantity] = STLINE.AMOUNT,
+        [Description] = STLINE.LINEEXP,
+        [IOType] = STLINE.IOCODE,
+        [WarehouseNumber] = CAPIWHOUSE.NR,
+        [WarehouseName] = CAPIWHOUSE.NAME
+        
+        FROM LG_{CompanyNumber}_{CompanyPeriod}_STLINE AS STLINE
+        LEFT JOIN LG_{CompanyNumber}_{CompanyPeriod}_STFICHE AS STFICHE ON STLINE.STFICHEREF = STFICHE.LOGICALREF
+        LEFT JOIN LG_{CompanyNumber}_ITEMS AS ITEMS ON STLINE.STOCKREF = ITEMS.LOGICALREF
+        LEFT JOIN LG_{CompanyNumber}_UNITSETL AS UNITSET ON STLINE.UOMREF = UNITSET.LOGICALREF
+        LEFT JOIN LG_{CompanyNumber}_UNITSETF AS BASEUNITSET ON STLINE.USREF = BASEUNITSET.LOGICALREF 
+        LEFT JOIN L_CAPIWHOUSE AS CAPIWHOUSE ON STLINE.SOURCEINDEX = CAPIWHOUSE.NR AND CAPIWHOUSE.FIRMNR = {Int32.Parse(CompanyNumber)}
+        ORDER BY STLINE.DATE_ DESC";
+
+		return query;
+	}
+
 	public void Dispose()
 	{
 		GC.SuppressFinalize(this);
--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/PanelViewModels/ProductPanelViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/PanelViewModels/ProductPanelViewModel.cs
@@ -1,7 +1,131 @@
-﻿using Helix.UI.Mobile.MVVMHelper;
+﻿using CommunityToolkit.Mvvm.Input;
+using Helix.UI.Mobile.Helpers.HttpClientHelper;
+using Helix.UI.Mobile.Helpers.MappingHelper;
+using Helix.UI.Mobile.Modules.BaseModule.Services;
+using Helix.UI.Mobile.Modules.ProductModule.Helpers.QueryHelper;
+using Helix.UI.Mobile.Modules.ProductModule.Models;
+using Helix.UI.Mobile.Modules.ProductModule.Views.ProductViews;
+using Helix.UI.Mobile.MVVMHelper;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace Helix.UI.Mobile.Modules.ProductModule.ViewModels.PanelViewModels;
 
 public partial class ProductPanelViewModel :BaseViewModel
 {
+	IHttpClientService _httpClientService;
+	ICustomQueryService _customQueryService;
+
+	public ObservableCollection<Product> Items { get; } = new();
+	public ObservableCollection<ProductTransactionLine> Lines { get; } = new();
+
+	public Command GetProductsCommand { get; }
+
+	public ProductPanelViewModel(ICustomQueryService customQueryService, IHttpClientService httpClientService)
+	{
+		_customQueryService = customQueryService;
+		_httpClientService = httpClientService;
+		GetProductsCommand = new Command(async () => await LoadData());
+		Title = "Malzeme Paneli";
+
+
+	}
+
+	[RelayCommand]
+	public async Task LoadData()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			await Task.Delay(300);
+			await Task.WhenAll(GetTopProductsAsync(), GetLastTransactionsAsync());
+
+
+
+
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Product Error: ", $"{ex.Message}", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	async Task GetTopProductsAsync()
+	{
+		try
+		{
+			var result = await _customQueryService.GetObjectsAsync(_httpClientService.GetOrCreateHttpClient(), new ProductQuery().GetTopProducts());
+			if (Items.Count>0)
+				Items.Clear();
+			
+			foreach (var item in result.Data)
+			{
+				var obj = Mapping.Mapper.Map<Product>(item);
+				if (obj.Image == "{}")
+				{
+					obj.Image = null;
+				}
+				Items.Add(obj);
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Waiting Sales Order Error: ", $"{ex.Message}", "Tamam");
+		}
+
+	}
+
+	async Task GetLastTransactionsAsync()
+	{
+		try
+		{
+			var result = await _customQueryService.GetObjectsAsync(_httpClientService.GetOrCreateHttpClient(), new ProductQuery().LastTransactionList());
+			foreach (var item in result.Data)
+			{
+				var obj = Mapping.Mapper.Map<ProductTransactionLine>(item);
+
+				Lines.Add(obj);
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Waiting Sales Order Error: ", $"{ex.Message}", "Tamam");
+		}
+
+	}
+
+	[RelayCommand]
+	public async Task GoToDetailAsync(Product product)
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			IsBusy = true;
+
+			await Task.Delay(300);
+			await Shell.Current.GoToAsync($"{nameof(ProductDetailView)}", new Dictionary<string, object>
+			{
+				[nameof(Product)] = product
+			});
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Error :", ex.Message, "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
 }
