ff86e9375bf8f03f86dcb6e0e2fc36486a456c09
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-01-02 17:28:17 +0300

    success sayfası

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViewModel/SuccessPageViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViewModel/SuccessPageViewModel.cs
@@ -1,12 +1,44 @@
-﻿using Helix.UI.Mobile.MVVMHelper;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Helix.UI.Mobile.MVVMHelper;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Helix.UI.Mobile.Modules.BaseModule.SharedViewModel;
 
-public class SuccessPageViewModel : BaseViewModel
+public partial class SuccessPageViewModel : BaseViewModel
 {
+	[ObservableProperty]
+	bool isAnimationStart;
+	public SuccessPageViewModel()
+	{
+		IsAnimationStart = false;
+	}
+
+	[RelayCommand]
+	public void StartAnimationHandler()
+	{
+		IsAnimationStart = true;
+	}
+
+	public async Task CloseButtonHandlerAsync()
+	{
+		try
+		{
+			IsBusy = true;
+			await Shell.Current.GoToAsync("..");
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
 }
