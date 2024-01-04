7b4781d5a5e0cbd253b6d14a27c3b2cf14694603
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-01-04 14:51:15 +0300

    Ürün giriş ve çıkış hareketleri datastore'u eklendi

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/DataStores/ProductTransactionLineDataStore.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/DataStores/ProductTransactionLineDataStore.cs
@@ -8,6 +8,7 @@ namespace Helix.UI.Mobile.Modules.ProductModule.DataStores;
 public class ProductTransactionLineDataStore : IProductTransactionLineService
 {
 	string postUrl = $"gateway/product/{nameof(ProductTransactionLine)}";
+
 	public async Task<DataResult<IEnumerable<ProductTransactionLine>>> GetTransactionLinesByProductCode(HttpClient httpClient, string code, string search, ProductTransactionLineOrderBy orderBy, int page, int pageSize)
 	{
 		HttpResponseMessage responseMessage = await httpClient.GetAsync($"{postUrl}/Product/Code/{code}?search={search}&orderBy={orderBy}&page={page}&pageSize={pageSize}");
@@ -123,6 +124,230 @@ public class ProductTransactionLineDataStore : IProductTransactionLineService
 	public async Task<DataResult<IEnumerable<ProductTransactionLine>>> GetTransactionLinesByTransactionType(HttpClient httpClient, string productCode, string transactionType, string search, ProductTransactionLineOrderBy orderBy, int page, int pageSize)
 	{
 		HttpResponseMessage responseMessage = await httpClient.GetAsync($"{postUrl}/ProductAndTransactionType/{productCode}&{transactionType}?search={search}&orderBy={orderBy}&page={page}&pageSize={pageSize}");
+		DataResult<IEnumerable<ProductTransactionLine>> dataResult = new DataResult<IEnumerable<ProductTransactionLine>>();
+
+		if (responseMessage.IsSuccessStatusCode)
+		{
+			var data = await responseMessage.Content.ReadAsStringAsync();
+			if (data != null)
+			{
+				if (!string.IsNullOrEmpty(data))
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "success";
+					return dataResult;
+				}
+				else
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "empty";
+					return dataResult;
+				}
+			}
+			else
+			{
+				var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+				{
+					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+				});
+				dataResult.Data = Enumerable.Empty<ProductTransactionLine>();
+				dataResult.IsSuccess = false;
+				dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+				return dataResult;
+			}
+		}
+		else
+		{
+			dataResult.Data = Enumerable.Empty<ProductTransactionLine>();
+			dataResult.IsSuccess = false;
+			dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+			return dataResult;
+		}
+	}
+
+	public async Task<DataResult<IEnumerable<ProductTransactionLine>>> GetInputTransactionLinesByProductCode(HttpClient httpClient, string code, string search, ProductTransactionLineOrderBy orderBy, int page, int pageSize)
+	{
+		HttpResponseMessage responseMessage = await httpClient.GetAsync($"{postUrl}/Product/Code/{code}/Input?search={search}&orderBy={orderBy}&page={page}&pageSize={pageSize}");
+		DataResult<IEnumerable<ProductTransactionLine>> dataResult = new DataResult<IEnumerable<ProductTransactionLine>>();
+
+		if (responseMessage.IsSuccessStatusCode)
+		{
+			var data = await responseMessage.Content.ReadAsStringAsync();
+			if (data != null)
+			{
+				if (!string.IsNullOrEmpty(data))
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "success";
+					return dataResult;
+				}
+				else
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "empty";
+					return dataResult;
+				}
+			}
+			else
+			{
+				var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+				{
+					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+				});
+				dataResult.Data = Enumerable.Empty<ProductTransactionLine>();
+				dataResult.IsSuccess = false;
+				dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+				return dataResult;
+			}
+		}
+		else
+		{
+			dataResult.Data = Enumerable.Empty<ProductTransactionLine>();
+			dataResult.IsSuccess = false;
+			dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+			return dataResult;
+		}
+	}
+
+	public async Task<DataResult<IEnumerable<ProductTransactionLine>>> GetInputTransactionLinesByProductId(HttpClient httpClient, int id, string search, ProductTransactionLineOrderBy orderBy, int page, int pageSize)
+	{
+		HttpResponseMessage responseMessage = await httpClient.GetAsync($"{postUrl}/Product/Id/{id}/Input?search={search}&orderBy={orderBy}&page={page}&pageSize={pageSize}");
+		DataResult<IEnumerable<ProductTransactionLine>> dataResult = new DataResult<IEnumerable<ProductTransactionLine>>();
+
+		if (responseMessage.IsSuccessStatusCode)
+		{
+			var data = await responseMessage.Content.ReadAsStringAsync();
+			if (data != null)
+			{
+				if (!string.IsNullOrEmpty(data))
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "success";
+					return dataResult;
+				}
+				else
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "empty";
+					return dataResult;
+				}
+			}
+			else
+			{
+				var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+				{
+					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+				});
+				dataResult.Data = Enumerable.Empty<ProductTransactionLine>();
+				dataResult.IsSuccess = false;
+				dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+				return dataResult;
+			}
+		}
+		else
+		{
+			dataResult.Data = Enumerable.Empty<ProductTransactionLine>();
+			dataResult.IsSuccess = false;
+			dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+			return dataResult;
+		}
+	}
+
+	public async Task<DataResult<IEnumerable<ProductTransactionLine>>> GetOutputTransactionLinesByProductCode(HttpClient httpClient, string code, string search, ProductTransactionLineOrderBy orderBy, int page, int pageSize)
+	{
+		HttpResponseMessage responseMessage = await httpClient.GetAsync($"{postUrl}/Product/Code/{code}/Output?search={search}&orderBy={orderBy}&page={page}&pageSize={pageSize}");
+		DataResult<IEnumerable<ProductTransactionLine>> dataResult = new DataResult<IEnumerable<ProductTransactionLine>>();
+
+		if (responseMessage.IsSuccessStatusCode)
+		{
+			var data = await responseMessage.Content.ReadAsStringAsync();
+			if (data != null)
+			{
+				if (!string.IsNullOrEmpty(data))
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "success";
+					return dataResult;
+				}
+				else
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "empty";
+					return dataResult;
+				}
+			}
+			else
+			{
+				var result = JsonSerializer.Deserialize<DataResult<IEnumerable<ProductTransactionLine>>>(data, new JsonSerializerOptions
+				{
+					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+				});
+				dataResult.Data = Enumerable.Empty<ProductTransactionLine>();
+				dataResult.IsSuccess = false;
+				dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+				return dataResult;
+			}
+		}
+		else
+		{
+			dataResult.Data = Enumerable.Empty<ProductTransactionLine>();
+			dataResult.IsSuccess = false;
+			dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+			return dataResult;
+		}
+	}
+
+	public async Task<DataResult<IEnumerable<ProductTransactionLine>>> GetOutputTransactionLinesByProductId(HttpClient httpClient, int id, string search, ProductTransactionLineOrderBy orderBy, int page, int pageSize)
+	{
+		HttpResponseMessage responseMessage = await httpClient.GetAsync($"{postUrl}/Product/Id/{id}/Output?search={search}&orderBy={orderBy}&page={page}&pageSize={pageSize}");
 		DataResult<IEnumerable<ProductTransactionLine>> dataResult = new DataResult<IEnumerable<ProductTransactionLine>>();
 
 		if (responseMessage.IsSuccessStatusCode)
--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/ProductViewModel/ProductDetailOutputTransactionListViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/ProductViewModel/ProductDetailOutputTransactionListViewModel.cs
@@ -71,7 +71,7 @@ public partial class ProductDetailOutputTransactionListViewModel : BaseViewModel
 			var httpClient = _httpClient.GetOrCreateHttpClient();
 
 			CurrentPage++;
-			var result = await _productTransactionLineService.GetTransactionLinesByTransactionType(httpClient, Product.Code, "2,3", SearchText, OrderBy, CurrentPage, PageSize);
+			var result = await _productTransactionLineService.GetOutputTransactionLinesByProductId(httpClient, Product.ReferenceId, SearchText, OrderBy, CurrentPage, PageSize);
 			if (result.Data.Any())
 			{
 				foreach (var item in result.Data)
@@ -110,7 +110,7 @@ public partial class ProductDetailOutputTransactionListViewModel : BaseViewModel
 			var httpClient = _httpClient.GetOrCreateHttpClient();
 			CurrentPage = 0;
 
-			var result = await _productTransactionLineService.GetTransactionLinesByTransactionType(httpClient, Product.Code, "2,3", SearchText, OrderBy, CurrentPage, PageSize);
+			var result = await _productTransactionLineService.GetOutputTransactionLinesByProductId(httpClient, Product.ReferenceId, SearchText, OrderBy, CurrentPage, PageSize);
 			if (result.Data.Any())
 			{
 				ProductOutputTransactionListItems.Clear();
