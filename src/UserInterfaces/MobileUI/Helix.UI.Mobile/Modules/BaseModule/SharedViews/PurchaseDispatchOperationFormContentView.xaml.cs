d585983fa8e74824b3fc41557a5f1f2a4bd3dffa
volkansunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-01-04 17:27:10 +0300

    dispatch update

---
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViews/PurchaseDispatchOperationFormContentView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Helix.UI.Mobile.Modules.BaseModule.SharedViews;
+
+public partial class PurchaseDispatchOperationFormContentView : ContentView
+{
+	public PurchaseDispatchOperationFormContentView()
+	{
+		InitializeComponent();
+	}
+}
