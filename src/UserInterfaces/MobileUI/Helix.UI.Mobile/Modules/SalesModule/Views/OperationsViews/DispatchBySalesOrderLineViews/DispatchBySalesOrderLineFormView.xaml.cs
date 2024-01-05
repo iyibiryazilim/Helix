60b05ac332550610b65bd20ce8286fba2a5a9853
Canberk Bekiroğlu <boom.canberk@gmail.com>
2024-01-05 17:16:38 +0300

    fix

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/SalesModule/Views/OperationsViews/DispatchBySalesOrderLineViews/DispatchBySalesOrderLineFormView.xaml.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/SalesModule/Views/OperationsViews/DispatchBySalesOrderLineViews/DispatchBySalesOrderLineFormView.xaml.cs
@@ -1,13 +1,9 @@
-using Helix.UI.Mobile.Modules.SalesModule.ViewModels.OperationsViewModels.DispatchBySalesOrderLineViewModels;
-
 namespace Helix.UI.Mobile.Modules.SalesModule.Views.OperationsViews.DispatchBySalesOrderLineViews;
 
 public partial class DispatchBySalesOrderLineFormView : ContentPage
 {
-    DispatchBySalesOrderLineFormViewModel _viewModel;
-    public DispatchBySalesOrderLineFormView(DispatchBySalesOrderLineFormViewModel viewModel)
+	public DispatchBySalesOrderLineFormView()
 	{
 		InitializeComponent();
-		BindingContext=_viewModel=viewModel;
 	}
 }
