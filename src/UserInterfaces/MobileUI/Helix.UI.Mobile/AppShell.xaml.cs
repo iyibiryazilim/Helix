e89e1f02d3f05a08eba3a78c86887352c5a54075
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-01-05 11:47:17 +0300

    Bekleyen Satış siparişleri

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/AppShell.xaml.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/AppShell.xaml.cs
@@ -96,6 +96,7 @@ public partial class AppShell : Shell
 		Routing.RegisterRoute(nameof(DispatchByPurchaseOrderLineListView), typeof(DispatchByPurchaseOrderLineListView));
 		Routing.RegisterRoute(nameof(DispatchByPurchaseOrderSummaryView), typeof(DispatchByPurchaseOrderSummaryView));
 		Routing.RegisterRoute(nameof(WarehouseTransactionContentView), typeof(WarehouseTransactionContentView));
+		Routing.RegisterRoute(nameof(SalesOrderLineContentView), typeof(SalesOrderLineContentView));
 	}
 }
 
