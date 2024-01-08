b12f05c472390b9214f6e02e4d79c68e2ea2568d
volkansunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-01-08 17:16:03 +0300

    driver update

---
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/SalesModule/Services/IDriverService.cs
@@ -0,0 +1,15 @@
+﻿using Helix.UI.Mobile.Modules.BaseModule.Dtos;
+using Helix.UI.Mobile.Modules.SalesModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helix.UI.Mobile.Modules.SalesModule.Services
+{
+    public interface IDriverService
+    {
+        Task<DataResult<IEnumerable<Driver>>> GetObjects(HttpClient httpClient);
+    }
+}
