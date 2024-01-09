4deff42c71c5cf0365a1bbf31493312479f29e32
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-01-09 10:55:03 +0300

    update WarehouseTransferOperation pages

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/OperationsViewModels/WarehouseTransferOperationViewModels/WarehouseTransferOperationViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/OperationsViewModels/WarehouseTransferOperationViewModels/WarehouseTransferOperationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Helix.UI.Mobile.Helpers.HttpClientHelper;
+using Helix.UI.Mobile.Modules.BaseModule.SharedViews;
 using Helix.UI.Mobile.Modules.ProductModule.Models;
 using Helix.UI.Mobile.Modules.ProductModule.Services;
 using Helix.UI.Mobile.MVVMHelper;
@@ -13,15 +14,19 @@ public partial class WarehouseTransferOperationViewModel : BaseViewModel
 {
 	IHttpClientService _httpClientService;
 	IWarehouseService _warehouseService;
+	IWarehouseTotalService _warehouseTotalService;
 
 	public ObservableCollection<Warehouse> WarehouseList { get; } = new();
+	public ObservableCollection<WarehouseTotal> Items { get; } = new();
 	public Command GetDataCommand { get; }
+	public Command<string> PerformSearchCommand { get; }
 	
-	public WarehouseTransferOperationViewModel(IHttpClientService httpClientService, IWarehouseService warehouseService)
+	public WarehouseTransferOperationViewModel(IHttpClientService httpClientService, IWarehouseService warehouseService, IWarehouseTotalService warehouseTotalService)
 	{
 		Title = "Ambar Transfer İşlemleri";
 		_httpClientService = httpClientService;
 		_warehouseService = warehouseService;
+		_warehouseTotalService = warehouseTotalService;
 
 		GetDataCommand  = new Command(async () => await LoadDataAsync());
 	}
@@ -34,7 +39,7 @@ public partial class WarehouseTransferOperationViewModel : BaseViewModel
 	[ObservableProperty]
 	int currentPage = 0;
 	[ObservableProperty]
-	int pageSize = 50;
+	int pageSize = 20;
 
 	async Task LoadDataAsync()
 	{
@@ -70,7 +75,7 @@ public partial class WarehouseTransferOperationViewModel : BaseViewModel
 
 			var httpClient = _httpClientService.GetOrCreateHttpClient();
 			CurrentPage = 0;
-			var result = await _warehouseService.GetObjects(httpClient, SearchText, DataStores.WarehouseDataStore.WarehouseOrderBy.numberasc, CurrentPage, PageSize);
+			var result = await _warehouseService.GetObjects(httpClient, SearchText, DataStores.WarehouseDataStore.WarehouseOrderBy.numberasc, CurrentPage, 50); // 50 is  PageSize
 
 			if(result.Data.Any())
 			{
@@ -93,4 +98,95 @@ public partial class WarehouseTransferOperationViewModel : BaseViewModel
 			IsRefreshing = false;
 		}
 	}
+
+	[RelayCommand]
+	public async Task GetSelectedWarehouseItemsAsync()
+	{
+		try
+		{
+			IsBusy = true;
+
+			var httpClient = _httpClientService.GetOrCreateHttpClient();
+			CurrentPage = 0;
+			//var result = await _warehouseTotalService.GetWarehouseTotals(httpClient,SelectedWarehouse.Number, "1,2,3,4,10,11,12,13")
+		}
+		catch(Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+			await Shell.Current.DisplayAlert("Hata", "Seçilen ambarın ürünleri getirilirken bir hata oluştu.", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+			IsRefreshing = false;
+		}
+	}
+
+
+	/*
+	[RelayCommand]
+	public async Task GoToBackAsync()
+	{
+		try
+		{
+			IsBusy = true;
+
+			if (Items.Count == 0)
+
+				await Shell.Current.GoToAsync("..");
+			else
+			{
+				bool answer = await Shell.Current.DisplayAlert("Uyarı", "Çıkmak İstediğinizden Emin misiniz", "Evet", "Hayır");
+				if (answer)
+				{
+					await Shell.Current.GoToAsync("..");
+					Items.Clear();
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex);
+			await Shell.Current.DisplayAlert("Hata", ex.Message, "Tamam");
+
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	[RelayCommand]
+	async Task GoToOperationFormAsync()
+	{
+		if (IsBusy)
+			return;
+
+		try
+		{
+			IsBusy = true;
+
+			if(Items.Count > 0)
+			{
+				await Shell.Current.GoToAsync($"{nameof()}", new Dictionary<string, object>
+				{
+					[nameof(ProductModel)] = Items
+				});
+			}
+			else
+			{
+				await Shell.Current.DisplayAlert("Hata", "Form sayfasına gitmek için ürününüzün bulunması gerekmektedir", "Tamam");
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+	*/
+
 }
