9d7bbe5f9f6c19961f01ffdb97650e7f567cf87a
volkansunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-01-10 16:29:24 +0300

    update

---
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ReturnModule/Views/Sales/ReturnBySalesDispatchTransactionLineViews/ReturnBySalesDispatchTransactionLineCustomerView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Helix.UI.Mobile.Modules.ReturnModule.Views.Sales.ReturnBySalesDispatchTransactionLineViews;
+
+public partial class ReturnBySalesDispatchTransactionLineCustomerView : ContentPage
+{
+	public ReturnBySalesDispatchTransactionLineCustomerView()
+	{
+		InitializeComponent();
+	}
+}
