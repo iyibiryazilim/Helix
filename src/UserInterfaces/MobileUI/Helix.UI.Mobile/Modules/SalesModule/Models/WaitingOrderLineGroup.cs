56d47debc60df380a923254e3ee656cf9ea341ef
Umut Sönmez <00umutsonmez00@gmail.com>
2024-01-16 17:15:34 +0300

    gruplama için model oluşturuldu.

---
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/SalesModule/Models/WaitingOrderLineGroup.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Helix.UI.Mobile.Modules.BaseModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helix.UI.Mobile.Modules.SalesModule.Models
+{
+    public partial class WaitingOrderLineGroup : ObservableObject
+    {
+        [ObservableProperty]
+        string code;
+
+        [ObservableProperty]
+        double stockQuantity;
+
+        [ObservableProperty]
+        double tempQuantity;
+
+        [ObservableProperty]
+        string name;
+
+        [ObservableProperty]
+        ObservableCollection<WaitingOrderLine> waitingOrderLines = new();
+    }
+}
