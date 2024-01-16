5506fe8dad17a079dbdeea4c1b3bca476eba336f
volkansunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-01-16 17:26:48 +0300

    özel kod alanları formlara eklendi.

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/OperationsViewModels/OutCountingTransactionOperationViewModels/OutCountingTransactionOperationFormViewModel.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/ProductModule/ViewModels/OperationsViewModels/OutCountingTransactionOperationViewModels/OutCountingTransactionOperationFormViewModel.cs
@@ -4,6 +4,8 @@ using Helix.UI.Mobile.Helpers.HttpClientHelper;
 using Helix.UI.Mobile.Modules.BaseModule.SharedViews;
 using Helix.UI.Mobile.Modules.ProductModule.Models;
 using Helix.UI.Mobile.Modules.ProductModule.Services;
+using Helix.UI.Mobile.Modules.SalesModule.Models;
+using Helix.UI.Mobile.Modules.SalesModule.Services;
 using Helix.UI.Mobile.MVVMHelper;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
@@ -18,6 +20,7 @@ public partial class OutCountingTransactionOperationFormViewModel : BaseViewMode
 {
     IHttpClientService _httpClientService;
     IWarehouseService _warehouseService;
+    ISpeCodeService _speCodeService;
     //WarehouseService
     public ObservableCollection<Warehouse> WarehouseItems { get; } = new();
 
@@ -41,17 +44,71 @@ public partial class OutCountingTransactionOperationFormViewModel : BaseViewMode
 
     [ObservableProperty]
     Warehouse warehouse;
+    //speCode
+    [ObservableProperty]
+    public string speCode = string.Empty;
+
+    public ObservableCollection<SpeCodeModel> SpeCodeModelItems { get; } = new();
 
 
-    public OutCountingTransactionOperationFormViewModel(IHttpClientService httpClientService, IWarehouseService warehouseService)
+    public OutCountingTransactionOperationFormViewModel(IHttpClientService httpClientService, IWarehouseService warehouseService, ISpeCodeService speCodeService)
     {
         Title = "Sayım Eksikliği İşlemleri";
         _httpClientService= httpClientService;
         _warehouseService= warehouseService;
+        _speCodeService= speCodeService;
         transactionTypeName = "Sayım Eksiği Fişi";
 
 
     }
+
+    [RelayCommand]
+    public async Task GetSpeCodeAsync()
+    {
+        string action;
+
+        try
+        {
+            var httpClient = _httpClientService.GetOrCreateHttpClient();
+            CurrentPage = 0;
+            var result = await _speCodeService.GetObjects(httpClient);
+
+            if (result.Data.Any())
+            {
+                SpeCodeModelItems.Clear();
+
+                foreach (var item in result.Data)
+                {
+                    SpeCodeModelItems.Add(item);
+                }
+
+                List<string> speCodeStrings = SpeCodeModelItems.Select(code => code.SpeCode).ToList();
+
+                action = await Shell.Current.DisplayActionSheet("Özel Kod:", "Vazgeç", null, speCodeStrings.ToArray());
+
+                SpeCode = action;
+
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert(" Error: ", $"{ex.Message}", "Tamam");
+        }
+        finally
+        {
+            IsBusy = false;
+
+        }
+    }
+
+
+
+
+
+
     [RelayCommand]
     public async Task GetWarehouseAsync()
     {
