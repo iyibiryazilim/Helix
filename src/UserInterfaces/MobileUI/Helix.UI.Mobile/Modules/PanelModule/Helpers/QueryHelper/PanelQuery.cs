17185ed174103dd5b7877bc02f9b3f7062dbb458
Umut Sönmez <00umutsonmez00@gmail.com>
2024-01-25 10:05:55 +0300

    firma 8 e çekildi

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/PanelModule/Helpers/QueryHelper/PanelQuery.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/PanelModule/Helpers/QueryHelper/PanelQuery.cs
@@ -8,7 +8,7 @@ namespace Helix.UI.Mobile.Modules.PanelModule.Helpers.QueryHelper;
 
 public class PanelQuery : IDisposable
 {
-	static string CompanyNumber = "003";
+	static string CompanyNumber = "008";
 	static string CompanyPeriod = "01";
 
 	public string GetTodayTransactionedProducts()
