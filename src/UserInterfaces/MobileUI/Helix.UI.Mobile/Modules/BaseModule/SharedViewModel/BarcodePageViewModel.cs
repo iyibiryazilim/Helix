37d53a8bbd44951ce62beb03d45343387121ea27
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-02-02 08:39:38 +0300

    Barkod eklendi

---
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViewModel/BarcodePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Helix.UI.Mobile.MVVMHelper;
+
+namespace Helix.UI.Mobile.Modules.BaseModule.SharedViewModel;
+
+public partial class BarcodePageViewModel : BaseViewModel
+{
+	public BarcodePageViewModel()
+	{
+		Title = "Barkod Okutma";
+	}
+
+}
