aff4efb9371976eeee5110de01c405f9bfa4624b
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-02-02 13:05:36 +0300

    Ambar transfer için barkod sayfası yapıldı

--- src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViews/BarcodePageView.xaml.cs
+++ src/UserInterfaces/MobileUI/Helix.UI.Mobile/Modules/BaseModule/SharedViews/BarcodePageView.xaml.cs
@@ -1,4 +1,7 @@
+using CommunityToolkit.Maui.Views;
+using Helix.UI.Mobile.Helpers.MessageHelper;
 using Helix.UI.Mobile.Modules.BaseModule.SharedViewModel;
+using Helix.UI.Mobile.Modules.ProductModule.ViewModels.OperationsViewModels.WarehouseTransferOperationViewModels;
 using System.Diagnostics;
 using ZXing;
 
@@ -7,14 +10,17 @@ namespace Helix.UI.Mobile.Modules.BaseModule.SharedViews;
 public partial class BarcodePageView : ContentPage
 {
 	BarcodePageViewModel _viewModel;
-	public BarcodePageView(BarcodePageViewModel viewModel)
+	IServiceProvider _serviceProvider;
+	public BarcodePageView(BarcodePageViewModel viewModel, IServiceProvider serviceProvider)
 	{
 		InitializeComponent();
 		BindingContext = _viewModel = viewModel;
+		_serviceProvider = serviceProvider;
 
 		cameraView.BarCodeOptions = new()
 		{
-			PossibleFormats = {ZXing.BarcodeFormat.QR_CODE, ZXing.BarcodeFormat.CODE_39, ZXing.BarcodeFormat.CODE_128, ZXing.BarcodeFormat.EAN_8}
+			PossibleFormats = {BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39, BarcodeFormat.CODE_128, BarcodeFormat.EAN_8, BarcodeFormat.CODE_39},
+			AutoRotate = true,
 		};
 	}
 
@@ -33,12 +39,27 @@ public partial class BarcodePageView : ContentPage
 		}
 	}
 
-	public void cameraView_BarcodeDetected(object sender, Camera.MAUI.ZXingHelper.BarcodeEventArgs args)
+	public void CameraView_BarcodeDetected(object sender, Camera.MAUI.ZXingHelper.BarcodeEventArgs args)
 	{
-		MainThread.BeginInvokeOnMainThread(() =>
+		MainThread.BeginInvokeOnMainThread(async () =>
 		{
-			Debug.WriteLine(args.Result[0].BarcodeFormat);
-			Debug.WriteLine(args.Result[0].Text);
+			CancellationTokenSource cancellationToken = new();
+			WarehouseTransferOperationViewModel viewModel = _serviceProvider.GetService<WarehouseTransferOperationViewModel>();
+			var product = viewModel.Items.Where(x => x.ProductCode == args.Result[0].Text).FirstOrDefault();
+			if(product != null)
+			{
+				product.IsSelected = true;
+				viewModel.SelectedItems.Add(product);
+				//Debug.WriteLine(args.Result[0].BarcodeFormat);
+				Debug.WriteLine(args.Result[0].Text);
+				await Task.Delay(500);
+				await Shell.Current.GoToAsync("..");
+			}
+			else
+			{
+				Debug.WriteLine(args.Result[0].Text);
+				await new MessageHelper().GetToastMessage("Ürün Bulunamadý").Show(cancellationToken.Token);
+			}
 		});
 	}
 }
