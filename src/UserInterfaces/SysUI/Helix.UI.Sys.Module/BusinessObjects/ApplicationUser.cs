10e0131b57188281b7a176f4cc4e51b651080a7c
volkansunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-02-19 15:52:25 +0300

    sys modul için bo eklendi.

--- src/UserInterfaces/SysUI/Helix.UI.Sys.Module/BusinessObjects/ApplicationUser.cs
+++ src/UserInterfaces/SysUI/Helix.UI.Sys.Module/BusinessObjects/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl.PermissionPolicy;
 using DevExpress.Xpo;
 
@@ -10,16 +11,43 @@ namespace Helix.UI.Sys.Module.BusinessObjects;
 [MapInheritance(MapInheritanceType.ParentTable)]
 [DefaultProperty(nameof(UserName))]
 public class ApplicationUser : PermissionPolicyUser, ISecurityUserWithLoginInfo {
+
+    private Department _department;
+    private Position _position;
+    private Employee _employee;
+    private ApplicationUserType _applicationUserType;
+
     public ApplicationUser(Session session) : base(session) { }
+
+    [ImmediatePostData]
+    public Department Department
+    {
+        get => _department;
+        set => SetPropertyValue(nameof(Department), ref _department, value);
+    }
+
+    [DataSourceProperty(nameof(Department))]
+    public Position Position
+    {
+        get => _position;
+        set => SetPropertyValue(nameof(Position), ref _position, value);
+    }
+
+    public Employee Employee
+    {
+        get => _employee;
+        set => SetPropertyValue(nameof(Employee), ref _employee, value);
+    }
+
+    public ApplicationUserType ApplicationUserType
+    {
+        get => _applicationUserType;
+        set => SetPropertyValue(nameof(ApplicationUserType), ref _applicationUserType, value);
+    }
+
 
     [Association("UserAssignGroup")]
     public XPCollection<ApplicationGroup> Groups => GetCollection<ApplicationGroup>(nameof(Groups));
-
-
-
-
-
-
 
 
 
---
+++ src/UserInterfaces/SysUI/Helix.UI.Sys.Module/BusinessObjects/TransactionOwner.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using static DevExpress.DataProcessing.InMemoryDataProcessor.AddSurrogateOperationAlgorithm;
+
+namespace Helix.UI.Sys.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // 
+    public class TransactionOwner : BaseObject
+    { 
+        private Employee _employee;
+        private int _ficheReferanceId;
+       
+
+        public TransactionOwner(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            
+        }
+        
+        public Employee Employee { get => _employee; set => SetPropertyValue(nameof(Employee), ref _employee, value); }
+
+        public int FicheReferanceId { get => _ficheReferanceId; set=>SetPropertyValue(nameof(FicheReferanceId),ref _ficheReferanceId, value); }
+  
+
+    }
+}
