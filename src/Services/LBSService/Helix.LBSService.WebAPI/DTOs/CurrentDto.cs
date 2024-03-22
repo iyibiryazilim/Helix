de52d20aa3da996b9c0ecf5338af8b2a029cc518
Canberk Bekiroğlu <boom.canberk@gmail.com>
2024-03-22 11:20:32 +0300

    Current Insert Fix

--- src/Services/LBSService/Helix.LBSService.WebAPI/DTOs/CurrentDto.cs
+++ src/Services/LBSService/Helix.LBSService.WebAPI/DTOs/CurrentDto.cs
@@ -2,18 +2,19 @@
 {
 	public class CurrentDto
 	{
- 		public string Title { get; set; } = string.Empty;
- 		public string Address { get; set; } = string.Empty;
+		public string Code { get; set; } = string.Empty;
+		public string Title { get; set; } = string.Empty;
+		public string Address { get; set; } = string.Empty;
 		public string OtherAddress { get; set; } = string.Empty;
 		public string DistrictCode { get; set; } = string.Empty;
- 		public string TownCode { get; set; } = string.Empty;
- 		public string CityCode { get; set; } = string.Empty;
- 		public string CountryCode { get; set; } = string.Empty;
- 		public string Telephone { get; set; } = string.Empty;
+		public string CountyCode { get; set; } = string.Empty;
+		public string CityCode { get; set; } = string.Empty;
+		public string CountryCode { get; set; } = string.Empty;
+		public string Telephone { get; set; } = string.Empty;
 		public string TaxNumber { get; set; } = string.Empty;
 		public string TaxOffice { get; set; } = string.Empty;
 		public string PaymentCode { get; set; } = string.Empty;
-		public string EMail { get; set; } = string.Empty; 
- 		 
-    }
+		public string EMail { get; set; } = string.Empty;
+		public string TCKN { get; set; } = string.Empty;
+	}
 }
