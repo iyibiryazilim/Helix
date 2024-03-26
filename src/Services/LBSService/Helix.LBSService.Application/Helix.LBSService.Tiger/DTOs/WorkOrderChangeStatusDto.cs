04f1fdc1c39df297a2ecddabaf1ca378a491355b
volkansunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-03-26 16:36:10 +0300

    workorder fix

--- src/Services/LBSService/Helix.LBSService.Application/Helix.LBSService.Tiger/DTOs/WorkOrderChangeStatusDto.cs
+++ src/Services/LBSService/Helix.LBSService.Application/Helix.LBSService.Tiger/DTOs/WorkOrderChangeStatusDto.cs
@@ -4,9 +4,7 @@
 	{
 		public string FicheNo { get; set; } = string.Empty;
 		public int Status { get; set; } = 0;
-		public short DeleteFiche { get; set; } = 2;
- 		public short ProductionType { get; set; } = 1;
-		public bool OpenTransaction { get; set; } = true;
+		public short DeleteFiche { get; set; } = 2; 
     }
 }
  
